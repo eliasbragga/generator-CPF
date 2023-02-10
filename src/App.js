943a6afecc0c8db41dcc8806567d72d86a6b413d
Elias notebook <eliasbragga44@gmail.com>
2023-02-09 21:37:34 -0300

    change h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Generator CPF</h1>
+        <h1>Mudou Ai ?</h1>
       </header>
     </div>
   );
