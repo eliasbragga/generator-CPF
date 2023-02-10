c5335273b99f27e1e947acfe50f71c7fecd89e0d
Elian Silva <you@example.com>
2023-02-09 21:44:55 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Generator CPF</h1>
+        <h2>Generator CPF</h2>
       </header>
     </div>
   );
